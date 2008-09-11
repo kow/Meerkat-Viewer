349636b4f835c3da7ee3089d0a867a127fe758e5
gigs <?@?.?>
2008-09-11 23:51:07 +0000

    version
    git-svn-id: http://meerkat-viewer.googlecode.com/svn/trunk@5 bd1948da-8ff0-11de-8ad7-8b2c2f1e2016

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -32,10 +32,10 @@
 #ifndef LL_LLVERSIONVIEWER_H
 #define LL_LLVERSIONVIEWER_H
 
-const S32 LL_VERSION_MAJOR = 1;
-const S32 LL_VERSION_MINOR = 21;
+const S32 LL_VERSION_MAJOR = 8;
+const S32 LL_VERSION_MINOR = 9;
 const S32 LL_VERSION_PATCH = 0;
-const S32 LL_VERSION_BUILD = 0;
+const S32 LL_VERSION_BUILD = 4;
 
 const char * const LL_CHANNEL = "Meerkat Release";
 
