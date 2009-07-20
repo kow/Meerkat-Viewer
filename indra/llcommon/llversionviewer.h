9da69e936905b147d8565703778596a24ea0d7d3
kow <patrick@sapinski.com>
2009-07-20 07:10:20 +0000

    version numbers for SVN rev 151
    git-svn-id: http://meerkat-viewer.googlecode.com/svn/trunk@152 bd1948da-8ff0-11de-8ad7-8b2c2f1e2016

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -35,7 +35,7 @@
 const S32 LL_VERSION_MAJOR = 0;
 const S32 LL_VERSION_MINOR = 1;
 const S32 LL_VERSION_PATCH = 4;
-const S32 LL_VERSION_BUILD = 142;
+const S32 LL_VERSION_BUILD = 151;
 
 const char * const LL_CHANNEL = "Meerkat Release";
 
